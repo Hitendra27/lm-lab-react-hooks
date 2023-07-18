801cc0d446735fd7656428b0ca85d9ace51664fc
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-18 18:06:45 +0400

    refactor: refactored useEffect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,30 +1,42 @@
 // make an api call using useEffect
 import { useEffect, useState } from "react";
+import { Todo } from "../use_x/todo";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const APICall = () => {
-  const [todo, setTodo] = useState(null);
+  const [todo, setTodo] = useState<Todo | null>(null);
   useEffect(() => {
     const fetchTodo = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      const data = await response.json();
-      setTodo(data);
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        const data = await response.json();
+        setTodo(data);
+      } catch (error) {
+        console.error("Error fetching todo:", error);
+      }
     };
-
     fetchTodo();
   }, []);
 
   return (
     <>
       <h2>useEffect</h2>
-      <p>
-        Todo ID: {todo.id}
-        <br />
-        Todo Title: {todo.title}
-        <br />
-        Todo Completed: {todo.completed.toString()}
-      </p>
+      {todo && (
+        <p>
+          Todo ID: {todo.id}
+          <br />
+          Todo Title: {todo.title}
+          <br />
+          Todo Completed: {todo.completed.toString()}
+        </p>
+      )}
     </>
   );
 };
