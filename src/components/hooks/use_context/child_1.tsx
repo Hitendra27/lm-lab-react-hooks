708d4f0928efd0f954b049688f5ad4d7ab7400ad
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-07-24 19:10:54 +0400

    refactor: refactoring useContext file

--- src/components/hooks/use_context/child_1.tsx
+++ src/components/hooks/use_context/child_1.tsx
@@ -1,10 +1,17 @@
-export const Child1: React.FC<{ dark: boolean }> = ({ dark }) => {
-	const theme = {
-		backgroundColor: dark ? '#333' : '#CCC',
-		color: dark ? 'white' : '#333',
-		padding: '2rem',
-		margin: '2rem',
-	};
+import { useContext } from "react";
+import { UserContext } from "./parent";
 
-	return <div style={theme}>Child 1</div>;
+export const Child1: React.FC<{ dark: boolean }> = ({ dark }) => {
+  const { dark } = useContext(UserContext);
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#CCC",
+    color: dark ? "white" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
+
+  return <div style={theme}>Child 1</div>;
 };
+
+export default Child1;
